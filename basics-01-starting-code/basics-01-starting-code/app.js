bb3950ccef159f09b9a78bf6afc8aecd344cab9c
Tran-Kevin-Dev <roy_kevin_tran@protonmail.com>
2022-07-16 18:26:29 +0200

    RNG version hardcoded

--- basics-01-starting-code/basics-01-starting-code/app.js
+++ basics-01-starting-code/basics-01-starting-code/app.js
@@ -2,11 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoal: "Finish the course and learn Vue!",
+      // rngGoal: this.outputGoal,
       vueLink: "https://github.com/",
     };
   },
   methods: {
-    
+    outputGoal() {
+      const rngNumber = Math.random();
+      if (rngNumber < 0.5) {
+        return "Become smart";
+      } else {
+        return "Cry in the shower";
+      }
+    },
   },
 });
 
