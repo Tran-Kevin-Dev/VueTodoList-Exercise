52a3a08ab3086ed14b2e0b8b94ae9edb9b2d0ec3
Tran-Kevin-Dev <roy_kevin_tran@protonmail.com>
2022-07-16 18:10:55 +0200

    first push

---
+++ basics-01-starting-code/basics-01-starting-code/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue!",
+      vueLink: "https://github.com/",
+    };
+  },
+  methods: {
+    
+  },
+});
+
+app.mount("#user-goal");
